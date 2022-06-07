1e33fe5f64d2bfb1b9562be994332c94dbe221aa
GET Academy <terje@getacademy.no>
2022-06-07 09:52:36 +0200

    Update Program.cs

--- SoManyQuestions/Program.cs
+++ SoManyQuestions/Program.cs
@@ -1,4 +1,12 @@
 ﻿
+var questions = new Question[] { 
+    new StringQuestion("Hva heter du"),
+    new IntQuestion("I hvilket år er du født"),
+    new SelectQuestion("Hvor fornøyd er du på en skala fra 1 til 6"),
+    new MultipleChoiceQuestion(),
+};
+
+
 // question 1
 var question1 = "Hva heter du? ";
 Console.Write(question1);
@@ -18,11 +26,19 @@ var answer3Str = Console.ReadLine();
 var isValid3 = int.TryParse(answer2Str, out int answer3) && answer3 >= 1 && answer3 <= 6;
 
 // question 4
-var options = new[] { "Ekstra bacon", "Ekstra ost", "Glutenfritt brød" };
+var options4 = new[] { "Ekstra bacon", "Ekstra ost", "Glutenfritt brød" };
 var question4 = "Vi du ha noe ekstra (eks: 1,2)? \n"
-    + string.Join("\n", options.Select((t, i) => $"{i + 1}: {t}"));
+    + string.Join("\n", options4.Select((t, i) => $"{i + 1}: {t}"));
 Console.Write(question4);
 var answer4Str = Console.ReadLine();
-var partsStr = answer4Str.Split(',');
-var partsInt = partsStr.Select(s => Convert.ToInt32(s)).ToArray();
-var isValid4 = partsInt.All(n => n >= 1 && n <= options.Length);
+var partsStr4 = answer4Str.Split(',');
+var partsInt4 = partsStr4.Select(s => Convert.ToInt32(s)).ToArray();
+var isValid4 = partsInt4.All(n => n >= 1 && n <= options4.Length);
+
+// question 5
+var options5 = new[] { "Rød", "Grønn", "Blå" };
+var question5 = "Vi du ha noe ekstra (eks: 1,2)? \n"
+    + string.Join("\n", options5.Select((t, i) => $"{i + 1}: {t}"));
+Console.Write(question4);
+var answer5Str = Console.ReadLine();
+var isValid5 = int.TryParse(answer5Str, out int answer5) && answer5 >= 1 && answer5 <= options5.Length;
